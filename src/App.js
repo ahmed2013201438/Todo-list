6b8e2df33e472651361ff905659b449e2f5b882c
Ahmed Alamodi <ahmed2013201438@gmail.com>
2021-06-06 13:00:18 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React about
+          Learn React about s
         </a>
       </header>
     </div>
